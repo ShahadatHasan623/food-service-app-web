ffca1dcc6371be9e56a16952bc97e34d3c6ebf88
ShahadatHasan623 <shahadathasan743599@gmail.com>
2025-08-23 02:52:16 +0600

    added loading page

---
+++ src/app/products/loading.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function loading() {
+  return (
+    <div className='flex justify-center items-center min-h-screen'><span className="loading loading-dots loading-xl"></span></div>
+  )
+}
